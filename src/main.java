20a12cff7f2c2e8feb990dea1a4cd12df20e9063
NIJZL00 <lotte.nijzink@outlook.com>
2021-09-20 12:20:47 +0200

    Second commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args){
-        System.out.println("This is a test");
+        System.out.println("This is a test in the branch");
     }
 }
